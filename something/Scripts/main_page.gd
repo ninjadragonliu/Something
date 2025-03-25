656d4da66a1e1d2aa7ff90a58cf7b19b067ed776
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-03-25 14:06:38 -0400

    update some buttons

--- something/Scripts/main_page.gd
+++ something/Scripts/main_page.gd
@@ -3,3 +3,7 @@ extends Control
 
 func _on_events_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/events.tscn")
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/overworld_map.tscn")
