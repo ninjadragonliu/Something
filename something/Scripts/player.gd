c1a6167154b7b069318a24896be12d121a7062df
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-03-27 20:44:42 -0400

    update player takes damage

--- something/Scripts/player.gd
+++ something/Scripts/player.gd
@@ -10,3 +10,9 @@ func take_damage(damage : int):
 	Global.health -= damage
 	if Global.health == 0:
 		queue_free()
+
+
+func _on_area_2d_body_entered(body: Node2D) -> void:
+	if body.is_in_group("enemy"):
+		self.take_damage(1)
+		body.queue_free()
