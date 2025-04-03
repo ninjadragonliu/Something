2e42524f83a2347c97809383842361faf8de58cb
GuanxunChen <jadkkddhdh@gmail.com>
2025-04-02 21:28:47 -0400

    enemyAmountLogicBase

--- something/Scripts/enemies.gd
+++ something/Scripts/enemies.gd
@@ -2,7 +2,14 @@ extends Node
 
 var enemies_num = 0
 
-@export var enemies_amount = 10
+@export var enemies_amount: int
+
+var level = 0
+var mapNumber = 1
+var mapConstant = 10 * mapNumber
+var growthFactor = 0.5
+var amplitude = 5 * min(mapNumber,100)
+
 var enemies_speed = 1
 var boss_speed = 1.5
 var spawn_duration = 0.1
@@ -15,6 +22,7 @@ var positions  = [
 ]
 
 func _ready() -> void:
+	enemies_amount = level * growthFactor + mapConstant + sin(level) * amplitude
 	#enemies = get_tree().get_nodes_in_group("enemy")
 	pass
 
