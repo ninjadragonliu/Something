6d147c863fe47517415feff74252a61e07437e61
GuanxunChen <jadkkddhdh@gmail.com>
2025-04-03 22:16:21 -0400

    algorithm update

--- something/Scripts/global.gd
+++ something/Scripts/global.gd
@@ -4,17 +4,24 @@ var max_health = 1
 var health = max_health
 var player_id
 var player_name = "John Doe"
+
 var level = 0
 var mapNumber = 1
-var mapConstant = 10 * mapNumber
-var growthFactor = 0.5
-var amplitude = 5 * min(mapNumber,100)
 var cleared_levels = []
 var levels_in_map = 11
+
 var tutorial_cleared = false
 var tutorial_levels = 5
 var cleared_tutorial_levels = []
 var tutorial_counter = 1
+
+var mapConstant_Amount = 20 * mapNumber
+var growthFactor_Amount = 2
+var amplitude_Amount = 7 * min(mapNumber, 100)
+
+var mapConstant_Speed = 1.2 * mapNumber
+var growthFactor_Speed = 0.03
+var amplitude_Speed = 0.1 * mapNumber
 
 func reset_health():
 	Global.health = Global.max_health
