8661e2cb3aa264b947625b46dba8bf91a3c59067
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-04-04 20:42:48 -0400

    added next level button funcstionality

--- something/Scripts/clear_level.gd
+++ something/Scripts/clear_level.gd
@@ -15,4 +15,13 @@ func _on_ok_pressed() -> void:
 
 
 func _on_next_level_pressed() -> void:
-	pass # Replace with function body.
+	if Global.level < Global.levels_in_map and Global.tutorial_cleared:
+		get_tree().paused = false
+		Global.level += 1
+		await get_tree().process_frame
+		get_tree().change_scene_to_file("res://Scenes/world.tscn")
+	else:
+		get_tree().paused = false
+		Global.tutorial_counter += 1
+		await get_tree().process_frame
+		get_tree().change_scene_to_file("res://Scenes/world.tscn")
