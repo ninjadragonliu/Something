d22dba484f19063fc132033f79909c59b4e4103f
GuanxunChen <jadkkddhdh@gmail.com>
2025-04-22 21:32:43 -0400

    animation_running
    some more animation editing

--- something/Scripts/enemy.gd
+++ something/Scripts/enemy.gd
@@ -1,16 +1,19 @@
 extends CharacterBody2D
 
 @onready var player = get_tree().get_nodes_in_group("player")[0]
+@onready var animatation = $AnimationPlayer
 var health = 1
 var speed = 1
 
 func _process(delta: float) -> void:
 	if player.position.x - position.x > 0:
 		position.x += speed
+		animatation.play("running_ToRight")
 		if position.x >= 279.0 and position.y == 150:
 			position.y = 400.0
 	elif player.position.x - position.x < 0:
 		position.x -= speed
+		animatation.play("running_ToLeft")
 		if position.x <= 873.0 and position.y == 150:
 			position.y = 400.0
 
