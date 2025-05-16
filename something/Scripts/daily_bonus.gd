208d47818ba7bb91bd7589ad71b68e84cdf8bcba
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-05-15 22:12:59 -0400

    added confirmation text

--- something/Scripts/daily_bonus.gd
+++ something/Scripts/daily_bonus.gd
@@ -3,5 +3,7 @@ extends Control
 signal daily_login_finished
 
 func _on_back_pressed() -> void:
+	$TextureRect.show()
+	await get_tree().create_timer(3.0).timeout
 	emit_signal("daily_login_finished")
 	queue_free()
