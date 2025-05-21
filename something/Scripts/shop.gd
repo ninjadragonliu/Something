4dfefaada4aa29c9fdb6beeb2705cae361ee2361
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-05-20 22:36:54 -0400

    added weapons and equipment section to shop scene

--- something/Scripts/shop.gd
+++ something/Scripts/shop.gd
@@ -9,8 +9,25 @@ func _on_back_pressed() -> void:
 func _on_costume_pressed() -> void:
 	$Costume.show()
 	$Skills.hide()
-
+	$Weapons.hide()
+	$Equipment.hide()
 
 func _on_skills_pressed() -> void:
 	$Costume.hide()
 	$Skills.show()
+	$Weapons.hide()
+	$Equipment.hide()
+
+
+func _on_weapons_pressed() -> void:
+	$Weapons.show()
+	$Costume.hide()
+	$Skills.hide()
+	$Equipment.hide()
+
+	
+func _on_equipment_pressed() -> void:
+	$Equipment.show()
+	$Weapons.hide()
+	$Skills.hide()
+	$Costume.hide()
