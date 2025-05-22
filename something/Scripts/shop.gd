063de409738c4bb9ccf46ec1dc37e2ffc7f6d6fa
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-05-21 21:00:47 -0400

    made changes to shop scene

--- something/Scripts/shop.gd
+++ something/Scripts/shop.gd
@@ -1,5 +1,7 @@
 extends Control
 
+func _ready() -> void:
+	$VBoxContainer/Costume.button_pressed = true
 
 func _on_back_pressed() -> void:
 	Global.save_game_data()
@@ -7,12 +9,20 @@ func _on_back_pressed() -> void:
 
 
 func _on_costume_pressed() -> void:
+	$VBoxContainer/Costume.button_pressed = true
+	$VBoxContainer/Skills.button_pressed = false
+	$VBoxContainer/Weapons.button_pressed = false
+	$VBoxContainer/Equipment.button_pressed = false
 	$Costume.show()
 	$Skills.hide()
 	$Weapons.hide()
 	$Equipment.hide()
 
 func _on_skills_pressed() -> void:
+	$VBoxContainer/Skills.button_pressed = true
+	$VBoxContainer/Costume.button_pressed = false
+	$VBoxContainer/Weapons.button_pressed = false
+	$VBoxContainer/Equipment.button_pressed = false
 	$Costume.hide()
 	$Skills.show()
 	$Weapons.hide()
@@ -20,6 +30,10 @@ func _on_skills_pressed() -> void:
 
 
 func _on_weapons_pressed() -> void:
+	$VBoxContainer/Weapons.button_pressed = true
+	$VBoxContainer/Skills.button_pressed = false
+	$VBoxContainer/Costume.button_pressed = false
+	$VBoxContainer/Equipment.button_pressed = false
 	$Weapons.show()
 	$Costume.hide()
 	$Skills.hide()
@@ -27,6 +41,10 @@ func _on_weapons_pressed() -> void:
 
 	
 func _on_equipment_pressed() -> void:
+	$VBoxContainer/Equipment.button_pressed = true
+	$VBoxContainer/Skills.button_pressed = false
+	$VBoxContainer/Costume.button_pressed = false
+	$VBoxContainer/Weapons.button_pressed = false
 	$Equipment.show()
 	$Weapons.hide()
 	$Skills.hide()
