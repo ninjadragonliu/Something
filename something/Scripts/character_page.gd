4d1b160d204149697aca49586ddf0a5179054343
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-06-03 21:19:22 -0400

    updated character page

--- something/Scripts/character_page.gd
+++ something/Scripts/character_page.gd
@@ -3,7 +3,10 @@ extends Control
 func _ready() -> void:
 	$Equipment/Weapon2.button_pressed = true
 	$Equipment.show()
+	$Skill.hide()
+	$Customize.hide()
 	$VBoxContainer/Equip.button_pressed = true
+	$Skill/Active.button_pressed = true
 
 func _on_back_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/main_page.tscn")
@@ -17,10 +20,11 @@ func _on_equip_pressed() -> void:
 	$VBoxContainer/Skill.button_pressed = false
 	$VBoxContainer/Customize.button_pressed = false
 
-
 func _on_skill_pressed() -> void:
 	$Equipment.hide()
 	$Skill.show()
+	$Skill/Active2.show()
+	$Skill/Passive2.hide()
 	$Customize.hide()
 	$VBoxContainer/Equip.button_pressed = false
 	$VBoxContainer/Skill.button_pressed = true
@@ -38,13 +42,34 @@ func _on_weapon_2_pressed() -> void:
 	$Equipment/Weapon.show()
 	$Equipment/Armor.hide()
 	$Equipment/Placeholder.hide()
+	$Equipment/Weapon2.button_pressed = true
+	$Equipment/Armor2.button_pressed = false
+	$Equipment/Placeholder2.button_pressed = false
 
 func _on_armor_2_pressed() -> void:
 	$Equipment/Weapon.hide()
 	$Equipment/Armor.show()
 	$Equipment/Placeholder.hide()
+	$Equipment/Weapon2.button_pressed = false
+	$Equipment/Armor2.button_pressed = true
+	$Equipment/Placeholder2.button_pressed = false
 
 func _on_placeholder_2_pressed() -> void:
 	$Equipment/Weapon.hide()
 	$Equipment/Armor.hide()
 	$Equipment/Placeholder.show()
+	$Equipment/Weapon2.button_pressed = false
+	$Equipment/Armor2.button_pressed = false
+	$Equipment/Placeholder2.button_pressed = true
+
+func _on_active_pressed() -> void:
+	$Skill/Active.button_pressed = true
+	$Skill/Passive.button_pressed = false
+	$Skill/Active2.show()
+	$Skill/Passive2.hide()
+
+func _on_passive_pressed() -> void:
+	$Skill/Active.button_pressed = false
+	$Skill/Passive.button_pressed = true
+	$Skill/Active2.hide()
+	$Skill/Passive2.show()
