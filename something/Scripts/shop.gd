f31801cc79d332c2c274c08778f1c426cd918861
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-06-05 21:50:04 -0400

    fixed stuff

--- something/Scripts/shop.gd
+++ something/Scripts/shop.gd
@@ -1,7 +1,7 @@
 extends Control
 
 func _ready() -> void:
-	$VBoxContainer/Featured.button_pressed = true
+	$ScrollContainer/VBoxContainer/Featured.button_pressed = true
 
 func _on_back_pressed() -> void:
 	Global.save_game_data()
@@ -9,11 +9,11 @@ func _on_back_pressed() -> void:
 
 
 func _on_costume_pressed() -> void:
-	$VBoxContainer/Costume.button_pressed = true
-	$VBoxContainer/Skills.button_pressed = false
-	$VBoxContainer/Weapons.button_pressed = false
-	$VBoxContainer/Equipment.button_pressed = false
-	$VBoxContainer/Featured.button_pressed = false
+	$ScrollContainer/VBoxContainer/Costume.button_pressed = true
+	$ScrollContainer/VBoxContainer/Skills.button_pressed = false
+	$ScrollContainer/VBoxContainer/Weapons.button_pressed = false
+	$ScrollContainer/VBoxContainer/Equipment.button_pressed = false
+	$ScrollContainer/VBoxContainer/Featured.button_pressed = false
 	$Costume.show()
 	$Skills.hide()
 	$Weapons.hide()
@@ -21,11 +21,11 @@ func _on_costume_pressed() -> void:
 	$Featured.hide()
 
 func _on_skills_pressed() -> void:
-	$VBoxContainer/Skills.button_pressed = true
-	$VBoxContainer/Costume.button_pressed = false
-	$VBoxContainer/Weapons.button_pressed = false
-	$VBoxContainer/Equipment.button_pressed = false
-	$VBoxContainer/Featured.button_pressed = false
+	$ScrollContainer/VBoxContainer/Costume.button_pressed = false
+	$ScrollContainer/VBoxContainer/Skills.button_pressed = true
+	$ScrollContainer/VBoxContainer/Weapons.button_pressed = false
+	$ScrollContainer/VBoxContainer/Equipment.button_pressed = false
+	$ScrollContainer/VBoxContainer/Featured.button_pressed = false
 	$Costume.hide()
 	$Skills.show()
 	$Weapons.hide()
@@ -34,11 +34,11 @@ func _on_skills_pressed() -> void:
 
 
 func _on_weapons_pressed() -> void:
-	$VBoxContainer/Weapons.button_pressed = true
-	$VBoxContainer/Skills.button_pressed = false
-	$VBoxContainer/Costume.button_pressed = false
-	$VBoxContainer/Equipment.button_pressed = false
-	$VBoxContainer/Featured.button_pressed = false
+	$ScrollContainer/VBoxContainer/Costume.button_pressed = false
+	$ScrollContainer/VBoxContainer/Skills.button_pressed = false
+	$ScrollContainer/VBoxContainer/Weapons.button_pressed = true
+	$ScrollContainer/VBoxContainer/Equipment.button_pressed = false
+	$ScrollContainer/VBoxContainer/Featured.button_pressed = false
 	$Weapons.show()
 	$Costume.hide()
 	$Skills.hide()
@@ -47,11 +47,11 @@ func _on_weapons_pressed() -> void:
 
 	
 func _on_equipment_pressed() -> void:
-	$VBoxContainer/Equipment.button_pressed = true
-	$VBoxContainer/Skills.button_pressed = false
-	$VBoxContainer/Costume.button_pressed = false
-	$VBoxContainer/Weapons.button_pressed = false
-	$VBoxContainer/Featured.button_pressed = false
+	$ScrollContainer/VBoxContainer/Costume.button_pressed = false
+	$ScrollContainer/VBoxContainer/Skills.button_pressed = false
+	$ScrollContainer/VBoxContainer/Weapons.button_pressed = false
+	$ScrollContainer/VBoxContainer/Equipment.button_pressed = true
+	$ScrollContainer/VBoxContainer/Featured.button_pressed = false
 	$Equipment.show()
 	$Weapons.hide()
 	$Skills.hide()
@@ -60,11 +60,11 @@ func _on_equipment_pressed() -> void:
 
 
 func _on_featured_pressed() -> void:
-	$VBoxContainer/Equipment.button_pressed = false
-	$VBoxContainer/Skills.button_pressed = false
-	$VBoxContainer/Costume.button_pressed = false
-	$VBoxContainer/Weapons.button_pressed = false
-	$VBoxContainer/Featured.button_pressed = true
+	$ScrollContainer/VBoxContainer/Costume.button_pressed = false
+	$ScrollContainer/VBoxContainer/Skills.button_pressed = false
+	$ScrollContainer/VBoxContainer/Weapons.button_pressed = false
+	$ScrollContainer/VBoxContainer/Equipment.button_pressed = false
+	$ScrollContainer/VBoxContainer/Featured.button_pressed = true
 	$Featured.show()
 	$Equipment.hide()
 	$Weapons.hide()
