1103397c0e54bf6e1d872cba01c126b8037996bb
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-06-13 15:57:44 -0400

    fixed dropdown menu, organized and cleaned up code

--- something/Scripts/shop.gd
+++ something/Scripts/shop.gd
@@ -12,18 +12,6 @@ func _on_back_pressed() -> void:
 	Global.save_game_data()
 	get_tree().change_scene_to_file("res://Scenes/main_page.tscn")
 
-func _on_costume_pressed1() -> void:
-	$ScrollContainer/VBoxContainer/Costume.button_pressed = true
-	$ScrollContainer/VBoxContainer/Skills.button_pressed = false
-	$ScrollContainer/VBoxContainer/Weapons.button_pressed = false
-	$ScrollContainer/VBoxContainer/Equipment.button_pressed = false
-	$ScrollContainer/VBoxContainer/Featured.button_pressed = false
-	$Costume.show()
-	$Skills.hide()
-	$Weapons.hide()
-	$Equipment.hide()
-	$Featured.hide()
-
 func _on_skills_pressed() -> void:
 	$ScrollContainer/VBoxContainer/Costume.button_pressed = false
 	$ScrollContainer/VBoxContainer/Skills.button_pressed = true
@@ -61,25 +49,114 @@ func _on_equipment_pressed() -> void:
 	$Featured.hide()
 
 func _on_featured_pressed() -> void:
-	$ScrollContainer/VBoxContainer/Costume.button_pressed = false
-	$ScrollContainer/VBoxContainer/Skills.button_pressed = false
-	$ScrollContainer/VBoxContainer/Weapons.button_pressed = false
-	$ScrollContainer/VBoxContainer/Equipment.button_pressed = false
-	$ScrollContainer/VBoxContainer/Featured.button_pressed = true
 	$Featured.show()
 	$Equipment.hide()
 	$Weapons.hide()
 	$Skills.hide()
 	$Costume.hide()
 
-func _on_costume_pressed() -> void:
-	if $ScrollContainer/VBoxContainer/Costume/Costume.button_down:
-		$ScrollContainer/VBoxContainer/Costume/Hair.show()
-		$ScrollContainer/VBoxContainer/Costume/Top.show()
-		$ScrollContainer/VBoxContainer/Costume/Bottom.show()
-		$ScrollContainer/VBoxContainer/Costume/Costume.button_pressed = true
+func _on_costume_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		#$Costume.show()
+		#$Featured.hide()
+		for child in $ScrollContainer/VBoxContainer/Costume.get_children():
+			child.show()
 	else:
-		$ScrollContainer/VBoxContainer/Costume/Hair.hide()
-		$ScrollContainer/VBoxContainer/Costume/Top.hide()
-		$ScrollContainer/VBoxContainer/Costume/Bottom.hide()
-		$ScrollContainer/VBoxContainer/Costume/Costume.button_pressed = false
+		#$Costume.hide()
+		for child in $Costume.get_children():
+			child.hide()
+		for child in $ScrollContainer/VBoxContainer/Costume.get_children():
+			if child.name.begins_with("Costume"):
+				continue
+			child.button_pressed = false
+			child.hide()
+
+func _on_skills_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		for child in $ScrollContainer/VBoxContainer/Skills.get_children():
+			child.show()
+	else:
+		for child in $Skills.get_children():
+			child.hide()
+		for child in $ScrollContainer/VBoxContainer/Skills.get_children():
+			if child.name.begins_with("Skills"):
+				continue
+			child.button_pressed = false
+			child.hide()
+
+func _on_weapons_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		for child in $ScrollContainer/VBoxContainer/Weapons.get_children():
+			child.show()
+	else:
+		for child in $Weapons.get_children():
+			child.hide()
+		for child in $ScrollContainer/VBoxContainer/Weapons.get_children():
+			if child.name.begins_with("Weapons"):
+				continue
+			child.button_pressed = false
+			child.hide()
+
+func _on_equipment_toggled(toggled_on: bool) -> void:
+	if toggled_on:
+		for child in $ScrollContainer/VBoxContainer/Equipment.get_children():
+			child.show()
+	else:
+		for child in $Equipment.get_children():
+			child.hide()
+		for child in $ScrollContainer/VBoxContainer/Equipment.get_children():
+			if child.name.begins_with("Equipment"):
+				continue
+			child.button_pressed = false
+			child.hide()
+
+func _on_hair_pressed() -> void:
+	$Costume.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Costume.get_children():
+		if child.name.begins_with("Costume"):
+			continue
+		elif child.name.begins_with("Hair"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Costume.get_children():
+		if child.name.begins_with("Hair"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_costop_pressed() -> void:
+	$Costume.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Costume.get_children():
+		if child.name.begins_with("Costume"):
+			continue
+		elif child.name.begins_with("CosTop"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Costume.get_children():
+		if child.name.begins_with("Top"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_cosbottom_pressed() -> void:
+	$Costume.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Costume.get_children():
+		if child.name.begins_with("Costume"):
+			continue
+		elif child.name.begins_with("CosBottom"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+	
+	for child in $Costume.get_children():
+		if child.name.begins_with("Bottom"):
+			child.show()
+			continue
+		child.hide()
