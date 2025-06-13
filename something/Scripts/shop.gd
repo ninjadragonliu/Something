caea471af555ea20cca6c508af411fb2c652e391
ninjadragonliu <142239451+ninjadragonliu@users.noreply.github.com>
2025-06-13 16:33:31 -0400

    worked on code

--- something/Scripts/shop.gd
+++ something/Scripts/shop.gd
@@ -18,6 +18,10 @@ func _on_featured_pressed() -> void:
 	$Weapons.hide()
 	$Skills.hide()
 	$Costume.hide()
+	for child in $ScrollContainer/VBoxContainer/Costume.get_children():
+		if child.name.begins_with("Costume"):
+			continue
+		child.button_pressed = false
 
 func _on_costume_toggled(toggled_on: bool) -> void:
 	if toggled_on:
@@ -120,6 +124,125 @@ func _on_cosbottom_pressed() -> void:
 		child.button_pressed = false
 	
 	for child in $Costume.get_children():
+		if child.name.begins_with("Bottom"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_active_pressed() -> void: # active skills
+	$Skills.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Skills.get_children():
+		if child.name.begins_with("Skills"):
+			continue
+		elif child.name.begins_with("Active"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Skills.get_children():
+		if child.name.begins_with("Active"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_passive_pressed() -> void: # passive skills
+	$Skills.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Skills.get_children():
+		if child.name.begins_with("Skills"):
+			continue
+		elif child.name.begins_with("Passive"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Skills.get_children():
+		if child.name.begins_with("Passive"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_fist_pressed() -> void:
+	$Weapons.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Weapons.get_children():
+		if child.name.begins_with("Weapons"):
+			continue
+		elif child.name.begins_with("Fist"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Weapons.get_children():
+		if child.name.begins_with("Fist"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_sword_pressed() -> void:
+	$Weapons.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Weapons.get_children():
+		if child.name.begins_with("Weapons"):
+			continue
+		elif child.name.begins_with("Sword"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Weapons.get_children():
+		if child.name.begins_with("Sword"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_lance_pressed() -> void:
+	$Weapons.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Weapons.get_children():
+		if child.name.begins_with("Weapons"):
+			continue
+		elif child.name.begins_with("Lance"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Weapons.get_children():
+		if child.name.begins_with("Lance"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_equip_top_pressed() -> void:
+	$Equipment.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Equipment.get_children():
+		if child.name.begins_with("Equipment"):
+			continue
+		elif child.name.begins_with("EquipTop"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Weapons.get_children():
+		if child.name.begins_with("Top"):
+			child.show()
+			continue
+		child.hide()
+
+func _on_equip_bottom_pressed() -> void:
+	$Equipment.show()
+	$Featured.hide()
+	for child in $ScrollContainer/VBoxContainer/Equipment.get_children():
+		if child.name.begins_with("Equipment"):
+			continue
+		elif child.name.begins_with("EquipBottom"):
+			child.button_pressed = true
+			continue
+		child.button_pressed = false
+
+	for child in $Weapons.get_children():
 		if child.name.begins_with("Bottom"):
 			child.show()
 			continue
